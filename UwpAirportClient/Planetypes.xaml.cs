4ced04886ae3546ec2dfa32859590415acc19618
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-07-29 18:26:05 +0300

    planetypes.xaml.cs implemented

--- UwpAirportClient/Planetypes.xaml.cs
+++ UwpAirportClient/Planetypes.xaml.cs
@@ -36,10 +36,10 @@ namespace UwpAirportClient
         {
             UpdateList();
             this.InitializeComponent();
-            this.Loaded += Tickets_Loaded;
+            this.Loaded += Planetypes_Loaded;
         }
 
-        private void Tickets_Loaded(object sender, RoutedEventArgs e)
+        private void Planetypes_Loaded(object sender, RoutedEventArgs e)
         {
             UpdateList();
         }
@@ -60,29 +60,56 @@ namespace UwpAirportClient
                 Frame.GoBack();
         }
 
-        private (Button, TextBox, TextBox) RenderCreate()
+        private (Button, TextBox, TextBox, TextBox) RenderCreate()
         {
             SingleItem.Children.Clear();
 
-            var number = new TextBox
+            var model = new TextBox
             {
-                Header = "Flight number",
+                Header = "Model",
                 Width = 200,
-                PlaceholderText = "Flight number",
+                PlaceholderText = "Model",
                 Margin = new Thickness(0, 100, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Center
             };
 
-            var price = new TextBox
+            var places = new TextBox
             {
-                Header = "Price ",
-                PlaceholderText = "Price",
+                Header = "Places ",
+                PlaceholderText = "Places",
                 Width = 200,
                 Margin = new Thickness(0, 10, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Center
             };
 
-            price.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
+            places.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
+            {
+                if (evArgs.Key.ToString().Equals("Back"))
+                {
+                    evArgs.Handled = false;
+                    return;
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (evArgs.Key.ToString() == string.Format("Number{0}", i))
+                    {
+                        evArgs.Handled = false;
+                        return;
+                    }
+                }
+                evArgs.Handled = true;
+            };
+
+            var carrying = new TextBox
+            {
+                Header = "Carrying",
+                PlaceholderText = "Carrying",
+                Width = 200,
+                Margin = new Thickness(0, 10, 0, 0),
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+
+            carrying.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
             {
                 if (evArgs.Key.ToString().Equals("Back"))
                 {
@@ -108,63 +135,90 @@ namespace UwpAirportClient
                 Margin = new Thickness(0, 20, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Center
             };
-            SingleItem.Children.Add(number);
-            SingleItem.Children.Add(price);
+            SingleItem.Children.Add(model);
+            SingleItem.Children.Add(places);
+            SingleItem.Children.Add(carrying);
             SingleItem.Children.Add(btnCreate);
-            return (btnCreate, number, price);
+            return (btnCreate, model, places, carrying);
         }
 
         private void Create_Click(object sender, RoutedEventArgs e)
         {
             var turple = RenderCreate();
             var btnCreate = turple.Item1;
-            var number = turple.Item2;
-            var price = turple.Item3;
+            var model = turple.Item2;
+            var places = turple.Item3;
+            var carrying = turple.Item4;
 
             btnCreate.Click += async (object sen, RoutedEventArgs evArgs) =>
             {
-                var ticketCreating = new PlaneTypeDTO()
-                { FlightNumber = number.Text, Price = double.Parse(price.Text) };
+                var planetype = new PlaneTypeDTO()
+                { Model = model.Text, Places = int.Parse(places.Text), Carrying = double.Parse(carrying.Text) };
                 try
                 {
-                    await service.CreateAsync(ticketCreating);
+                    await service.CreateAsync(planetype);
                 }
                 catch (Exception) { }
 
-                planetypesList.Add(ticketCreating);
+                planetypesList.Add(planetype);
                 UpdateList();
                 SingleItem.Children.Clear();
             };
         }
 
-        private (Button, Button, TextBox, TextBox, PlaneTypeDTO) RenderDetail(ItemClickEventArgs e)
+        private (Button, Button, TextBox, TextBox,TextBox, PlaneTypeDTO) RenderDetail(ItemClickEventArgs e)
         {
-            var ticket = (PlaneTypeDTO)e.ClickedItem;
+            var planetype = (PlaneTypeDTO)e.ClickedItem;
             SingleItem.Children.Clear();
 
             var id = new TextBlock();
-            id.Text = "Ticket id:" + ticket.Id;
+            id.Text = "Planetype id:" + planetype.Id;
             id.FontWeight = Windows.UI.Text.FontWeights.Bold;
             id.Margin = new Thickness(0, 100, 10, 10);
             id.Width = 200;
 
-            var number = new TextBox
+            var model = new TextBox
             {
-                Header = "Flight number",
-                Text = ticket.FlightNumber,
+                Header = "Model",
+                Text = planetype.Model,
                 Width = 200,
                 HorizontalAlignment = HorizontalAlignment.Center
             };
 
 
-            var price = new TextBox
+            var places = new TextBox
             {
                 Header = "Price ",
-                Text = ticket.Price.ToString(),
+                Text = planetype.Places.ToString(),
                 Width = 200,
                 HorizontalAlignment = HorizontalAlignment.Center
             };
-            price.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
+            places.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
+            {
+                if (evArgs.Key.ToString().Equals("Back"))
+                {
+                    evArgs.Handled = false;
+                    return;
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (evArgs.Key.ToString() == string.Format("Number{0}", i))
+                    {
+                        evArgs.Handled = false;
+                        return;
+                    }
+                }
+                evArgs.Handled = true;
+            };
+
+            var carrying = new TextBox
+            {
+                Header = "Price ",
+                Text = planetype.Carrying.ToString(),
+                Width = 200,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            carrying.KeyDown += (object obj, KeyRoutedEventArgs evArgs) =>
             {
                 if (evArgs.Key.ToString().Equals("Back"))
                 {
@@ -201,13 +255,14 @@ namespace UwpAirportClient
                 Margin = new Thickness(0, 20, 0, 0)
             };
             SingleItem.Children.Add(id);
-            SingleItem.Children.Add(number);
-            SingleItem.Children.Add(price);
+            SingleItem.Children.Add(model);
+            SingleItem.Children.Add(places);
+            SingleItem.Children.Add(carrying);
             buttonsStack.Children.Add(btnUpdate);
             buttonsStack.Children.Add(btnDelete);
             SingleItem.Children.Add(buttonsStack);
 
-            return (btnUpdate, btnDelete, number, price, ticket);
+            return (btnUpdate, btnDelete, model, places,carrying, planetype);
         }
 
         private void itemsList_ItemClick(object sender, ItemClickEventArgs e)
@@ -215,21 +270,22 @@ namespace UwpAirportClient
             var turple = RenderDetail(e);
             var btnUpdate = turple.Item1;
             var btnDelete = turple.Item2;
-            var number = turple.Item3;
-            var price = turple.Item4;
-            var ticket = turple.Item5;
+            var model = turple.Item3;
+            var places = turple.Item4;
+            var carrying = turple.Item5;
+            var planetype = turple.Item6;
 
             btnUpdate.Click += async (object sen, RoutedEventArgs evArgs) =>
             {
-                var ticketUpdating = new PlaneTypeDTO()
-                { Id = ticket.Id, FlightNumber = number.Text, Price = double.Parse(price.Text) };
+                var planetypeCreating = new PlaneTypeDTO()
+                { Id = planetype.Id, Model = model.Text, Places = int.Parse(places.Text), Carrying = int.Parse(carrying.Text)};
 
-                int index = planetypesList.ToList().FindIndex(t => t.Id == ticket.Id);
-                planetypesList.Insert(index, ticketUpdating);
+                int index = planetypesList.ToList().FindIndex(t => t.Id == planetype.Id);
+                planetypesList.Insert(index, planetypeCreating);
 
                 try
                 {
-                    await service.UpdateAsync(ticketUpdating);
+                    await service.UpdateAsync(planetypeCreating);
                 }
                 catch (Exception) { }
 
@@ -239,11 +295,11 @@ namespace UwpAirportClient
 
             btnDelete.Click += async (object sen, RoutedEventArgs evArgs) =>
             {
-                planetypesList.Remove(ticket);
+                planetypesList.Remove(planetype);
 
                 try
                 {
-                    await service.DeleteAsync(ticket);
+                    await service.DeleteAsync(planetype);
                 }
                 catch (Exception) { }
                 UpdateList();
