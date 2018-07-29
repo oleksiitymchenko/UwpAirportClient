66ae1e9daa4e19a454f38aa9aaf284f6e3bc88ba
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-07-30 02:12:56 +0300

    removed unnecessary usings

--- UwpAirportClient/Crews.xaml.cs
+++ UwpAirportClient/Crews.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UwpAirportClient.Models;
 using UwpAirportClient.Services;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -51,6 +42,7 @@ namespace UwpAirportClient
         {
             UpdateList();
             GetStews();
+            GetPilots();
         }
 
         private async void GetPilots()
@@ -119,6 +111,7 @@ namespace UwpAirportClient
             var addticketButton = new Button()
             {
                 Content = "Add stewardess",
+                HorizontalAlignment = HorizontalAlignment.Center
             };
             addticketButton.Click += (object sendet, RoutedEventArgs e) =>
             {
--- UwpAirportClient/MainPage.xaml.cs
+++ UwpAirportClient/MainPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -49,7 +38,7 @@ namespace UwpAirportClient
 
         private void CrewsClick(object sender, RoutedEventArgs e)
         {
-            Ticketbtn.Content = "Hello UWP!";
+            Frame.Navigate((typeof(Crews)));
         }
 
         private void FlightsClick(object sender, RoutedEventArgs e)
